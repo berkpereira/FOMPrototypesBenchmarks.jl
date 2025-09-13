713c091124963ccf7fe731436604aaee4a0b0b02
Gabriel Pereira <berkpereira@gmail.com>
2025-09-13 18:10:45 +0100

    changed defaults

--- src/defaults.jl
+++ src/defaults.jl
@@ -1,27 +1,34 @@
 const DEFAULT_SOLVER_ARGS = Dict{String,Any}(
     # reference solver
     "ref-solver"            => :SCS,
+    
     # method‐defining defaults
-    "variant"               => :ADMM,
+    "variant"               => :PDHG,
     "res-norm"              => Inf,
-    "acceleration"          => :none,
     "rho"                   => 1.0,
     "theta"                 => 1.0,
+    
+    "acceleration"          => :none,
     "accel-memory"          => 10,
+    "safeguard-norm"        => :euclid, # in {:euclid, :char, :none}
+    
+    "krylov-tries-per-mem"  => 1,
     "krylov-operator"       => :tilde_A,
-    "anderson-period"       => 2,
+    
+    "anderson-interval"     => 10,
     "anderson-broyden-type" => :normal2, # in {Symbol(1), :normal2, :QR2}
     "anderson-mem-type"     => :rolling, # in {:rolling, :restarted}
     "anderson-reg"          => :none, # in {:none, :tikonov, :frobenius}
+    
     # non‐defining defaults
     "max-iter"              => Inf,
     "rel-kkt-tol"           => 1e-6,
-    "print-mod"             => 5000,
+    "print-mod"             => 10_000,
     "print-res-rel"         => true,
     "show-vlines"           => false,
     "run-fast"              => true,
-	"global-timeout"        => 60.0, # include set-up time (seconds)
-	"loop-timeout"          => Inf,  # exclude set-up time (seconds)
+	"global-timeout"        => 120.0, # include set-up time (seconds)
+	"loop-timeout"          => Inf,   # exclude set-up time (seconds)
     
 	# not in use
     "restart-period"        => Inf,
