0b84bfc738a7da4c0984d778ece6d9b3242f3f75
Gabriel Pereira <berkpereira@gmail.com>
2025-10-08 17:05:30 +0100

    adjusts histogram ticks

--- analysis/spmv_plots.jl
+++ analysis/spmv_plots.jl
@@ -2,14 +2,14 @@ include("spmv_utils.jl")
 
 problem_sets = String[
     "sslsq",
-    # "mpc",
-    # "maros",
-    # "netlib_feaisble"
+    "mpc",
+    "maros",
+    # "netlib_feasible"
 ]
 
 gr()
 
-time_metric = :median # in {:min, :median, :max}
+time_metric = :min # in {:min, :median, :max}
 df = load_spmv_results(; problem_sets=problem_sets, time_metric=time_metric)
 rf = ratios_by_op(df; time_metric=time_metric)
 out_dir = joinpath(dirname(@__DIR__), "analysis", "figs", "spmv")
@@ -39,16 +39,30 @@ hist_st = paper_plot_kwargs(
     xlim=(-Inf, Inf),
 )
 
+FILTER_NONSENSE = true
+# filter out ratios below 1
+if FILTER_NONSENSE
+    rf_hist = filter(:ratio_cr => x -> ismissing(x) || x ≥ 1.0, rf)
+    @info "Filtered out $(size(rf, 1) - size(rf_hist, 1)) records for histogram."
+else
+    rf_hist = rf
+end
+
+lo, hi = extrema(skipmissing(rf_hist.ratio_cr))
+tick_start = 0.5 * floor(lo / 0.5)
+tick_stop  = 0.5 * ceil(hi / 0.5)
+
 hist_plt = plot_ratio_hist(
-    rf;
+    rf_hist;
     column=:single,
     fontsize=5,
     fontfamily="Computer Modern",
     tight=true,
     by_op=false,
-    # bins=30,
+    bins=50,
     normalize=:probability,
     alpha=0.9,
+    xticks = tick_start:0.25:tick_stop,
     outfile=joinpath(out_dir, "spmv_cr_ratio_hist.pdf"),
     hist_st...
 )
